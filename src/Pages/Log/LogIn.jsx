7b036ac1d96ba246f3a0d35f5c523733be4b968a
nhnishat <nhnishat1998@gmail.com>
2023-05-02 03:07:59 +0600

    regiser & show password

--- src/Pages/Log/LogIn.jsx
+++ src/Pages/Log/LogIn.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LogIn = () => {
+	const [showPassword, setShowPassword] = useState(false);
+	const handleTogglePassword = () => {
+		setShowPassword((prevShowPassword) => !prevShowPassword);
+	};
 	return (
 		<Container className="w-25 mx-auto mt-5 p-5">
-			<h3 className="mb-4 text-center">Log in</h3>
+			<h1 className="mb-4 text-center">Log in</h1>
 			<Form>
 				<Form.Group className="mb-3" controlId="formBasicEmail">
 					<Form.Label>Email address</Form.Label>
@@ -19,16 +24,26 @@ const LogIn = () => {
 				<Form.Group className="mb-3" controlId="formBasicPassword">
 					<Form.Label>Password</Form.Label>
 					<Form.Control
-						type="password"
+						type={showPassword ? 'text' : 'password'}
 						name="password"
 						placeholder="Password"
 						required
 					/>
 				</Form.Group>
-				<Button variant="primary" type="submit">
+				<Link onClick={handleTogglePassword} className="my-2 text-black ">
+					{showPassword ? 'Hide' : 'Show'} password
+				</Link>
+				<br />
+				<Button variant="primary" type="submit" className="my-2">
 					Login
 				</Button>
 			</Form>
+			<p>
+				Dont’t Have An Account ?
+				<Link to="/register" className="text-danger ms-2">
+					Register
+				</Link>
+			</p>
 		</Container>
 	);
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 	return (
-		<div>
-			<h1>this is register</h1>
-		</div>
+		<Container className="w-25 mx-auto mt-5 p-5">
+			<h1 className="mb-4 text-center">Register</h1>
+			<Form>
+				<Form.Group className="mb-3" controlId="formBasicName">
+					<Form.Label>Name</Form.Label>
+					<Form.Control
+						type="text"
+						name="name"
+						placeholder="Enter Name"
+						required
+					/>
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="formBasicPhoto">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						type="text"
+						name="phono Url"
+						placeholder="Enter Phono Url"
+						required
+					/>
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						type="email"
+						name="email"
+						placeholder="Enter email"
+						required
+					/>
+				</Form.Group>
+
+				<Form.Group className="mb-3" controlId="formBasicPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type="password"
+						name="password"
+						placeholder="Password"
+						required
+					/>
+				</Form.Group>
+				<Button variant="primary" type="submit" className="mb-2">
+					Register
+				</Button>
+				<p>
+					Already Have An Account ?
+					<Link to="/login" className="text-danger ms-2">
+						Login
+					</Link>
+				</p>
+			</Form>
+		</Container>
 	);
 };
 
