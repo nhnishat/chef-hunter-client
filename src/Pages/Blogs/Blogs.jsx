cbaaa13ff346d85fa223d7abedcb86147f5af365
nhnishat <nhnishat1998@gmail.com>
2023-05-04 19:12:38 +0600

    blog done

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -4,8 +4,8 @@ import { Container } from 'react-bootstrap';
 const Blogs = () => {
 	return (
 		<Container>
-			<h1 className="mt-5 text-center">Question & Answer </h1>
-			<div className="shadow-lg p-3 rounded-4">
+			<h1 className="my-5 text-center">Question & Answer </h1>
+			<div className="shadow-lg p-3 rounded-4 my-5">
 				<h3 className="fw-semibold">
 					Tell us the differences between uncontrolled and controlled
 					components?
@@ -44,7 +44,7 @@ const Blogs = () => {
 					</ul>
 				</p>
 			</div>
-			<div className="shadow-lg p-4 rounded-4">
+			<div className="shadow-lg p-4 rounded-4 my-5">
 				<h4 className="fw-bold">
 					How to validate React props using PropTypes?
 				</h4>
@@ -53,6 +53,39 @@ const Blogs = () => {
 					PropTypes package and define the expected data type and shape of the
 					props in your component. This helps catch bugs and errors early on and
 					ensures that the component receives the correct props.
+				</p>
+			</div>
+			<div className="shadow-lg p-4 rounded-4 my-5">
+				<h4>Tell us the difference between nodejs and express js?</h4>
+				<p>
+					Node.js is a runtime environment for executing JavaScript code outside
+					of a web browser, while Express.js is a framework for building web
+					applications in Node.js. Node.js provides a platform for running
+					JavaScript on the server-side, allowing developers to use JavaScript
+					for building backend applications, whereas Express.js provides a set
+					of tools for building web applications such as routing, middleware,
+					and templating. While Node.js is the core runtime environment for
+					JavaScript, Express.js is an extension of Node.js, adding web
+					application functionality to Node.js. In other words, Node.js can be
+					used to build a variety of applications, while Express.js is focused
+					on building web applications specifically.
+				</p>
+			</div>
+			<div className="shadow-lg p-4 rounded-4 my-5">
+				<h4>What is a custom hook, and why will you create a custom hook?</h4>
+				<p>
+					A custom hook is a reusable function in React that encapsulates
+					specific logic or behavior and can be shared across multiple
+					components. Custom hooks are useful in scenarios where you need to
+					reuse complex logic, state management, or side effects in multiple
+					components. Custom hooks allow you to create self-contained
+					functionality that is easier to test, maintain and modify. By
+					encapsulating complex logic or behavior, you can reduce the amount of
+					repetitive code and avoid cluttering your components with unnecessary
+					code. Some examples of custom hooks include managing form input state,
+					handling authentication, caching data, and fetching data from an API.
+					Overall, custom hooks are a powerful and flexible tool that can help
+					make your React code more modular, reusable and maintainable.
 				</p>
 			</div>
 		</Container>
